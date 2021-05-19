26b81f944fd478e77cfe3b0bcb5c1bf6d88e7b9f
Carlos Huerta Garcia <huerta2502@gmail.com>
2021-05-19 17:44:30 -0500

    models from class diagram began

---
+++ project/model/User.js
@@ -0,0 +1,24 @@
+class User {
+    constructor(id, name, pswd, type, addr) {
+        this.id = id;
+        this.name = name;
+        this.pswd = pswd;
+        this.type = type;
+        this.addr = addr;
+    }
+    login(name, pswd){
+
+    }
+    signin(name, pswd, type, addr) {
+
+    }
+    read(id) {
+
+    }
+    update(id, name, pswd, type, addr) {
+
+    }
+    delete(id) {
+        
+    }
+}
