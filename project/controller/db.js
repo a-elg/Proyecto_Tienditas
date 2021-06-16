f60e5bf17ccffce9aa7e4c8fff8ec894aea56ee9
A-ELG <angellopezgracia@gmail.com>
2021-06-16 18:53:25 -0500

    Back up

--- project/controller/db.js
+++ project/controller/db.js
@@ -16,13 +16,9 @@ function query(lit_query){
             console.log("Sucesfully connected to server.");
             connection.query(lit_query,(err,result)=>{
                 if(err) throw err;
-                console.log(`Affected Rows: ${result.affectedRows}`);
                 console.log(`Result:`);
                 console.log(result);
-                console.log(`result[0]:`);
-                console.log(result[0]);
-                console.log(`result[0][0]:`);
-                console.log(result[0][0]);
+                return 
             });
         });
     } catch (error) {
@@ -32,7 +28,7 @@ function query(lit_query){
 }module.exports.query=query;
 
 
-//Customer________________________________________________________________________________________________________________
+// Save user________________________________________________________________________________________________________________
 function saveCustomer(email,name,phone,password){
     try {
         connection.connect((err)=>{
@@ -49,6 +45,7 @@ function saveCustomer(email,name,phone,password){
 }module.exports.saveCustomer=saveCustomer;
 
 
+// Signin_____________________________________________________________________________________________
 function signinCustomer(email,password){
     try {
         connection.connect((err)=>{
@@ -65,11 +62,89 @@ function signinCustomer(email,password){
     }   
 }module.exports.signinCustomer=signinCustomer;
 
+function signinStore(email,password){
+    try {
+        connection.connect((err)=>{
+            if(err) throw err;
+            connection.query(`call signinStore('${email}','${password}')`,(err,result)=>{
+                if(err) throw err;
+                return result[0][0].casesignin;
+            });
+        });
+    } catch (error) {
+        console.log("Error:");
+        console.log(error);
+        return 0;
+    }    
+}module.exports.signinStore=signinStore;
+
+function signinDelivery(email,password){
+    try {
+        connection.connect((err)=>{
+            if(err) throw err;
+            connection.query(`call signinDelivery('${email}','${password}')`,(err,result)=>{
+                if(err) throw err;
+                return result[0][0].casesignin;
+            });
+        });
+    } catch (error) {
+        console.log("Error:");
+        console.log(error);
+        return 0;
+    }    
+}module.exports.signinDelivery=signinDelivery;
+
+function signinAdmin(email,password){
+    try {
+        connection.connect((err)=>{
+            if(err) throw err;
+            connection.query(`call signinAdmin('${email}','${password}')`,(err,result)=>{
+                if(err) throw err;
+                return result[0][0].casesignin;
+            });
+        });
+    } catch (error) {
+        console.log("Error:");
+        console.log(error);
+        return 0;
+    }    
+}module.exports.signinAdmin=signinAdmin;
+
+// Read____________________________________________________________________________________________
+function readCustomer(email){
+    try {
+        connection.connect((err)=>{
+            if(err) throw err;
+            connection.query(`call readCustomer('${email}')`,(err,result)=>{
+                if(err) throw err;
+                let query_result=result[0][0];
+                let user=new users.Customer(
+                        query_result.c_email,
+                        query_result.c_name,
+                        query_result.c_password,
+                        "customer",
+                        null,
+
+                    );
+                return user;
+            });
+        });
+    } catch (error) {
+        console.log("Error:");
+        console.log(error);
+        return 0;
+    } 
+}module.exports.readCustomer=readCustomer;
 
 
+query(`call readCustomer('correo2')`);
 
 
+// User operations_________________________________________________________________________________________________
 
+function getShoppingCart(email){
+    let query_result=query(`select * from shopping_carts where c_mail=${email};`);
+}
 
 
 console.log("db.js correctly set");
